9fed7d480ba278a3490fac046fa756ce10901141
Liz <minethur3@gmail.com>
2026-03-14 00:16:25 -0300

    Delete resources directory

--- resources/modules/data.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-inline bool running = true;
-inline char cpubuff[255];
-inline char rambuff[255];
-inline char freediskspace[255];
-inline char diskspace[255];
-inline float wtOpacity = 1;
-inline bool alwaysOnTop = true;
