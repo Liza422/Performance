7b7c656059d5534946988557a41abdaece4c023b
Liz <minethur3@gmail.com>
2026-03-13 23:03:24 -0300

    Delete src directory

--- src/resources/modules/data.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-inline bool running = true;
-inline bool enabled = true;
-inline char cpubuff[255];
-inline char rambuff[255];
-inline char freediskspace[255];
-inline char diskspace[255];
-inline float wtOpacity = 1;
